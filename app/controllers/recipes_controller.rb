298b61736a4d2ec454a8d8165c59ee262593f447
Veronica Lopez <nicaa0695@gmail.com>
2019-09-27 17:37:22 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -19,5 +19,7 @@ class RecipesController < ApplicationController
   
   private 
   
-  
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
+  end 
 end
